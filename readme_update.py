250041195c7e2c79be06fbff2e3fe29aaf9b2637
동석 <dongseok@dongseog-ui-Macmini.local>
2025-03-25 23:44:19 +0900

    [modify] readme_update.py

--- readme_update.py
+++ readme_update.py
@@ -2,13 +2,13 @@ import feedparser
 import ssl
 import os
 
-# 로컬 테스트 시 ssl 인증서 문제 해결용
+# SSL 인증서 문제 해결용
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# rss 추출
+# RSS 피드 파싱
 feed = feedparser.parse("https://striver.tistory.com/rss")
 
-# README 양식
+# README 템플릿
 markdown_text = """
 ###  🐱 github stats  
 
@@ -28,18 +28,9 @@ markdown_text = """
 # 최근 블로그 추가
 for i in feed["entries"][:10]:
     markdown_text += f"<a href =\"{i['link']}\"> {i['title']} </a> <br>\n"
-    # print(i['link'], i['title'])
 
-# 기존 README.md 파일 내용과 비교
-current_content = ""
-if os.path.exists("README.md"):
-    with open("README.md", "r", encoding="utf-8") as f:
-        current_content = f.read()
-
-# 내용이 변경된 경우에만 파일 업데이트
-if current_content != markdown_text:
-    with open("README.md", "w", encoding="utf-8") as f:
-        f.write(markdown_text)
-    print("README.md 파일이 업데이트되었습니다.")
-else:
-    print("블로그 포스트에 변경사항이 없습니다.")
+# 항상 새 내용 작성
+with open("README.md", "w", encoding="utf-8") as f:
+    f.write(markdown_text)
+    
+print("README.md가 최신 블로그 포스트로 업데이트되었습니다.")
